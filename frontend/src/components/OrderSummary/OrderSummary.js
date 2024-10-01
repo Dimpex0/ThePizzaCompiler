c565ffc4579f2d437e9f9cc5453a2f08160918ca
Dimitar Dimitrov <mitkodd2005@gmail.com>
2024-10-01 10:38:27 +0300

    added order summary component for cart page; started cart item responsive style

---
+++ frontend/src/components/OrderSummary/OrderSummary.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useCartStore } from "../../store/cart";
+
+import "./OrderSummary.css";
+
+export default function OrderSummary() {
+  const { cart } = useCartStore();
+  const [addressField, setAddressField] = useState(false);
+  const [address, setAddress] = useState("");
+
+  let totalPrice = 0;
+
+  for (const cartItem of cart) {
+    if (cartItem.selectedSize) {
+      totalPrice += Number(
+        cartItem.item[`${cartItem.selectedSize}_price`] * cartItem.quantity
+      );
+    } else {
+      totalPrice += Number(cartItem.price);
+    }
+  }
+
+  function handleChange(e) {
+    console.log(e.target.value);
+    if (e.target.value === "delivery") {
+      setAddressField(true);
+    } else {
+      setAddressField(false);
+    }
+  }
+
+  return (
+    <>
+      <p className="shipping">Shipping</p>
+      <select onChange={handleChange}>
+        <option value="pick-up">Pick up</option>
+        <option value="delivery">Delivery</option>
+      </select>
+      {addressField && (
+        <input
+          name="address"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+          placeholder="Address..."
+        />
+      )}
+      <div className="total-container">
+        <p>Total cost</p>
+        <p>{totalPrice.toFixed(2)}$</p>
+        <button>Checkout</button>
+      </div>
+    </>
+  );
+}
--- frontend/src/pages/Cart/Cart.js
+++ frontend/src/pages/Cart/Cart.js
@@ -3,11 +3,12 @@ import { useCartStore } from "../../store/cart";
 import CartItem from "../../components/CartItem/CartItem";
 
 import "./Cart.css";
+import OrderSummary from "../../components/OrderSummary/OrderSummary";
 
 export default function CartPage() {
   const { cart } = useCartStore();
   return (
-    <>
+    <div className="page-container">
       <section className="cart-section">
         <div className="header-section">
           <h1>Shopping cart</h1>
@@ -20,6 +21,12 @@ export default function CartPage() {
             ))}
         </ul>
       </section>
-    </>
+      <section className="order-section">
+        <div className="header-section">
+          <h1>Order summary</h1>
+        </div>
+        <OrderSummary />
+      </section>
+    </div>
   );
 }
