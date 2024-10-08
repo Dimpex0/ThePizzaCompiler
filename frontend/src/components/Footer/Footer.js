fb9c7b47429e00e01bc929506b11c7516f22deed
HP 830 G5 <mitkodd2005@gmail.com>
2024-10-08 22:07:28 +0300

    added footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,5 +1,46 @@
 import React from "react";
+import logoImg from "../../assets/images/logo/Pizza slice with brackets.png";
+
+import "./Footer.css";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <footer>
+      <img src={logoImg} alt="logo" />
+      <h4>The pizza compiler</h4>
+      <div className="footer-container">
+        <div className="footer-legal">
+          <p>Legal pages</p>
+          <Link to="/legal/privacy-policy">Privacy Policy</Link>
+          <Link to="/legal/terms-and-conditions">Terms and Conditions</Link>
+          <Link to="/legal/cookie-policy">Cookie Policy</Link>
+          <Link to="/legal/legal-disclaimer">Legal Disclaimer</Link>
+        </div>
+        <div className="divider"></div>
+        <div className="footer-contacts">
+          <p>Contacts</p>
+          <div className="footer-icons">
+            <a
+              href="https://github.com/Dimpex0"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa-brands fa-github"></i>
+            </a>
+            <a href="mailto:mitkodd2005@gmail.com">
+              <i className="fa-solid fa-envelope"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/dimitar-dimitrov-441834267/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- frontend/src/pages/Root/Root.js
+++ frontend/src/pages/Root/Root.js
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import MainNavigation from "../../components/MainNavigation/MainNavigation";
+import Footer from "../../components/Footer/Footer";
 
 export default function RootPage() {
   return (
     <>
       <MainNavigation />
       <Outlet />
+      <Footer />
     </>
   );
 }
